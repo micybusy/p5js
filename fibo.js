70ecf7246fa3e0356af03734a12009117a3bdd90
Harun Kılıç <harunlokmank@gmail.com>
2023-06-20 01:04:06 +0300

    new files.

---
+++ fibo.js
@@ -0,0 +1,28 @@
+let x1, x2, r, g, b, alpha, wh, start, stop, extra_canvas;
+function setup() {
+    createCanvas(window.innerWidth-20, window.innerHeight-20);
+    x1 = width/2
+    x2 = height/2
+    wh = 100
+    start = 0.1
+    stop  = 0.2
+  }
+
+  function draw() {
+    fill(r, g, b, alpha)
+    arc(x1, x2, wh, wh, start, stop, PIE)
+    start = stop
+    stop+=0.1
+    wh*=1.005
+    if (wh >= height){
+      noLoop()
+    }
+    
+}
+
+    function mousePressed(){
+      r = random(255);
+      g = random(255);
+      b = random(255);
+      alpha = random(255);
+    }
---
+++ hi.js
@@ -0,0 +1,34 @@
+let x, y, hit_low_right, hit_up_left, x_factor, y_factor;
+function setup() {
+    createCanvas(window.innerWidth-20, window.innerHeight-20);
+    x = width/2;
+    y = height/2;
+    hit_low_right = false
+    hit_up_left = false
+    x_factor=random(5, 40)
+    y_factor=random(5, 40)
+
+}
+
+  function draw() {
+    background(255)
+    fill(120, 120, 120)
+    circle(x, y, 100)
+    x += x_factor
+    y += y_factor
+
+    if (x >= width-50 ){
+        x_factor *= -1;}
+    else if (x<=50)
+        {x_factor *= -1;
+        }
+
+    if (y >= height-50 ){
+        y_factor *= -1;}
+    else if (y<=50)
+            {y_factor *= -1;
+            }
+    console.log(x)
+    console.log(y)
+    
+}
