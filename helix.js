efee18eb1d988826433493f0225c9300c338b221
Harun Kılıç <harun.kilic@hangikredi.com>
2023-06-27 10:40:13 +0300

    helix done.

--- helix.js
+++ helix.js
@@ -1,15 +1,17 @@
 let x, y, z, t, r, h;
 function setup(){
-    createCanvas(innerWidth-20, innerHeight-20)
-    x = 0; y = 0; z = 0; t = 200; r = 200; h = 500;
+    createCanvas(innerWidth-20, innerHeight-20, WEBGL)
+    x = 0; y = 0; z = 0; t = 0; r = 300; h = 10;
+    background(0)
 }
 
 
 function draw(){
-    // TODO: Add Z rotation to the helix to see the depth of it. 
-    x = r*Math.cos(t) + 500;
-    y = r*Math.sin(t) + 500;
-    z = h*t + 500;
+    z = r*Math.cos(t);
+    x = r*Math.sin(t);
+    y = h*t -250
+    stroke(255) 
+    strokeWeight(5)
     point(x, y, z)
     t += 0.01
 }
